028e1ca0430733e039c1a343752950179f2edab6
meinhoonharsh <harshprogrammer782@gmail.com>
2021-08-11 12:47:25 +0530

    Basic Functionality and Route Added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from os import environ
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import json
 import datetime
+
+
 
 def getUserData(route):
     if request.headers.getlist("X-Forwarded-For"):
@@ -21,6 +24,21 @@ app = Flask(__name__)
 app.debug = True
 app.port = 8000
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/urlshortner'
+db = SQLAlchemy(app)
+
+
+#Connecting database to table Contact
+class URL(db.Model):
+    '''
+    sno, name phone_num, msg, date, email
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), nullable=False)
+    url = db.Column(db.String(512), nullable=False)
+    timestamp = db.Column(db.String(12), nullable=True)
+
 # 404 Handling
 @app.errorhandler(404)
 def not_found(e):
@@ -32,6 +50,16 @@ def not_found(e):
 def index():
     getUserData("Index Page")
     return render_template('index.html')
+
+@app.route('/<slug>')
+def short(slug):
+    try:
+        url = URL.query.filter_by(slug=slug).first()
+        getUserData(request.path)
+        return redirect(url.url)
+    except:
+        getUserData(request.path)
+        return render_template('pages/404.html')
 
 if __name__ == '__main__':
     app.run()
